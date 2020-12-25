41be523c609827309fe7828c7cec3efa46714447
bryce <brycekbargar@gmail.com>
2020-12-25 16:51:01 -0600

    Implement super naive jwt auth for login and get

--- backend/ports/echohttp/server.go
+++ backend/ports/echohttp/server.go
@@ -2,6 +2,7 @@ package echohttp
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -10,19 +11,22 @@ import (
 // Start starts the given server after performing Echo specific setup.
 func Start(port int, secret string) error {
 	s := echo.New()
+	s.Use(middleware.Logger())
+	s.Use(middleware.Recover())
 
 	key := []byte(secret)
-
 	fullAuth := middleware.JWTWithConfig(middleware.JWTConfig{
 		SigningKey: key,
+		AuthScheme: "Token",
 	})
-
 	maybeAuth := middleware.JWTWithConfig(middleware.JWTConfig{
 		SigningKey: key,
+		AuthScheme: "Token",
 		Skipper: func(c echo.Context) bool {
 			// Partially auth'd endpoints have different behavior when the user is logged in
 			// We want to make sure that only truly anon requests skip auth in these scenarios
-			return len(c.Request().Header.Get("Authorization")) == 0
+			auth := c.Request().Header.Get("Authorization")
+			return len(auth) == 0 || len(strings.TrimPrefix(auth, "Token ")) == 0
 		},
 	})
 
--- backend/ports/echohttp/users.go
+++ backend/ports/echohttp/users.go
@@ -1,6 +1,12 @@
 package echohttp
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
+)
 
 type userHandler struct {
 	authed      echo.MiddlewareFunc
@@ -30,17 +36,68 @@ func (r *userHandler) routes(s *echo.Echo) {
 	s.DELETE("/profile/:name/follow", r.unfollow, r.authed)
 }
 
+type user struct {
+	User userUser `json:"user"`
+}
+type userUser struct {
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+	Username string `json:"username"`
+	Bio      string `json:"bio"`
+	Image    string `json:"image"`
+}
+
 func (r *userHandler) create(c echo.Context) (err error) {
 	return nil
 }
 
-func (r *userHandler) login(c echo.Context) (err error) {
-	return nil
+type login struct {
+	User loginUser `json:"user"`
+}
+type loginUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
+func (r *userHandler) login(c echo.Context) (err error) {
+	l := new(login)
+	if err = c.Bind(l); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	if l.User.Email != "jon@jonsnow.com" || l.User.Password != "shhh!" {
+		return echo.ErrUnauthorized
+	}
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = "JonSnow"
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	t, err := token.SignedString(r.key)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, user{
+		userUser{
+			Email:    l.User.Email,
+			Token:    t,
+			Username: claims["name"].(string),
+		},
+	})
 }
 
 func (r *userHandler) user(c echo.Context) (err error) {
-	return nil
+	ju := c.Get("user").(*jwt.Token)
+	claims := ju.Claims.(jwt.MapClaims)
+	return c.JSON(http.StatusOK, user{
+		userUser{
+			Token:    ju.Raw,
+			Username: claims["name"].(string),
+		},
+	})
 }
 func (r *userHandler) update(c echo.Context) (err error) {
 	return nil
