8cd3e3737e5839627fb31f82b43a924b6d15525d
bryce <brycekbargar@gmail.com>
2020-12-25 14:30:01 -0600

    Throw in a bunch of unused auth/user related endpoints

--- backend/ports/echohttp/server.go
+++ backend/ports/echohttp/server.go
@@ -19,5 +19,14 @@ func (s *Server) Start(port int) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
+	s.server.POST("/users", nil)       // create (anon)
+	s.server.POST("/users/login", nil) // login (anon)
+	s.server.GET("/user", nil)         // get (auth'd only and current user)
+	s.server.PUT("/user", nil)         // update (auth'd only and current user)
+
+	s.server.GET("/profile/:name", nil)           // get (auth'd or anonymous w/ same results)
+	s.server.GET("/profile/:name/follow", nil)    // get (auth'd only and current user)
+	s.server.DELETE("/profile/:name/follow", nil) // get (auth'd only and current user)
+
 	return s.server.Start(":" + strconv.Itoa(port))
 }
