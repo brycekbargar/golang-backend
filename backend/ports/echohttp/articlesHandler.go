3093e0d924d3f024782e80ed2cd0049084dc0892
bryce <brycekbargar@gmail.com>
2021-01-16 14:21:45 -0600

    Add the initial json response models

--- backend/ports/echohttp/articlesHandler.go
+++ backend/ports/echohttp/articlesHandler.go
@@ -1,6 +1,9 @@
 package echohttp
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/labstack/echo/v4"
 
 	"github.com/brycekbargar/realworld-backend/ports"
@@ -27,13 +30,41 @@ func (r *articlesHandler) mapRoutes(g *echo.Group) {
 	g.GET("/articles", r.list, r.maybeAuthed)
 }
 
+type author struct {
+	Username  string `json:"username"`
+	Bio       string `json:"bio"`
+	Image     string `json:"image"`
+	Following bool   `json:"following"`
+}
+
+type article struct {
+	Slug           string    `json:"slug"`
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+	Body           string    `json:"body"`
+	TagList        []string  `json:"tagList"`
+	CreatedAt      time.Time `json:"createdAt"`
+	UpdatedAt      time.Time `json:"updatedAt"`
+	Favorited      bool      `json:"favorited"`
+	FavoritesCount int       `json:"favoritesCount"`
+	Author         author    `json:"author"`
+}
+
+type list struct {
+	Articles      []article `json:"articles"`
+	ArticlesCount int       `json:"articlesCount"`
+}
+
 func (r *articlesHandler) list(c echo.Context) error {
 	em, _, _ := c.(*userContext).identity()
 
 	// get all articles
 	if len(em) > 0 {
-		// set the following logic?
+		// set the following/favorited logic?
 	}
 
-	return nil
+	return c.JSON(http.StatusOK, list{
+		make([]article, 0),
+		0,
+	})
 }
